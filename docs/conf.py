424dc61637a59cdf44fd800d58e502db6c2cbbbb
Vignir Isberg <vigniri@gmail.com>
2015-10-07 21:51:42 +0200

    Updated docs config

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'GPCRdb'
-copyright = '2015, Vignir Isberg'
+copyright = '2015, GPCRdb'
 author = 'Vignir Isberg'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -109,7 +109,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
